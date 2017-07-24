69ae234fa3e76d8947ab004c7e8fbb1dd457af8d
Paulo Lopes <pcslopes@gmail.com>
2017-07-24 12:04:41 +1200

    heros

---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
